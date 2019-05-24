5c3f2a5f6a1b1b562acef91e60aa986da3adf030
Михаил Тырсына <mishatvent@gmail.com>
2019-05-24 18:06:38 +0500

    Update README.md

---
+++ core/components/reachgoal/lexicon/en/default.inc.php
@@ -0,0 +1,29 @@
+<?php
+include 'settings.inc.php';
+
+$_lang['reachgoal'] = 'ReachGoal';
+$_lang['reachgoal_menu_desc'] = 'Управление целями';
+
+$_lang['reachgoal_panel_main'] = 'Цели';
+$_lang['reachgoal_panel_main_desc'] = 'Тут отображается список целей';
+$_lang['reachgoal_goals_grid_search_empty'] = 'Поиск';
+
+$_lang['reachgoal_service_metrika'] = 'Яндекс.Метрика';
+$_lang['reachgoal_service_gtag'] = 'Global Site Tag';
+$_lang['reachgoal_service_gtm'] = 'Google Tag Manager';
+
+$_lang['reachgoal_types_addproduct'] = 'Добавление в корзину';
+$_lang['reachgoal_types_removeproduct'] = 'Удаление из корзины';
+$_lang['reachgoal_types_order'] = 'Оформление заказа';
+$_lang['reachgoal_types_ajaxform'] = 'Отправка формы (AjaxForm)';
+
+$_lang['reachgoal_goals_grid_service'] = 'Сервис';
+$_lang['reachgoal_goals_grid_form_id'] = 'Форма';
+$_lang['reachgoal_err_form_id_null'] = 'Напишите <b>id</b> формы!';
+$_lang['reachgoal_goals_grid_empty_form_id'] = 'Введите идентификатор формы (html-атрибут id)';
+$_lang['reachgoal_goals_grid_service_id'] = 'Номер счетчика';
+$_lang['reachgoal_goals_grid_empty_service_id'] = 'По умолчанию будет использоваться Системная настройка reachgoal_yacounter_default';
+$_lang['reachgoal_err_service_id_null'] = 'Заполните <b>Номер счетчика</b>!';
+$_lang['reachgoal_goals_grid_event'] = 'Событие';
+$_lang['reachgoal_goals_grid_goal_name'] = 'Цель / Действие';
+$_lang['reachgoal_goals_grid_goal_category'] = 'Категория';
